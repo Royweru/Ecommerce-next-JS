cb919cbce1770f55d9ed29e4a56e8ad4646e4e0e
Royweru <royezi17@gmail.com>
2023-10-15 14:53:51 +0300

    modified:   app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx 	new file:   app/(auth)/layout.tsx 	modified:   app/(root)/page.tsx 	modified:   app/layout.tsx 	new file:   components/modals/store-modal.tsx 	new file:   components/ui/dialog.tsx 	new file:   components/ui/modal.tsx 	new file:   hooks/useStoreModal.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/schema.prisma 	new file:   providers/modal-provider.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,25 @@
+"use client"
+
+import {useEffect} from 'react'
+
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/useStoreModal";
 const SetupPage = ()=> {
+  const onOpen= useStoreModal((state)=>state.onOpen)
+  const isOpen= useStoreModal((state)=>state.isOpen)
+
+  useEffect(()=>{
+   if(!isOpen){
+    onOpen()
+   }
+  },[isOpen,onOpen])
   return (
     <main className="flex min-h-screen flex-col  justify-start p-2">
-     <div className=" text-lg font-bold">
-      <UserButton afterSignOutUrl="/"/>
+     <div className="p-4">
+      RootPage
      </div>
-    
     </main>
   )
 }
